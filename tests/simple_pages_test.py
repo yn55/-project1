b9569075b095eb068f3b713c53bec200f10a8aae
yoshi <yn55@njit.edu>
2022-02-28 20:06:46 -0500

    Renaming Links and updated titles

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,10 +5,10 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about!">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">Page 1</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/python_flask">Python and Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/contid">Continuous Integration and Deployment</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
@@ -22,29 +22,29 @@ def test_request_about(client):
     assert response.status_code == 200
     assert b"About Page" in response.data
 
-def test_request_page1(client):
+def test_request_git(client):
     """This makes the index page"""
-    response = client.get("/page1")
+    response = client.get("/git")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"Git" in response.data
 
-def test_request_page2(client):
+def test_request_docker(client):
     """This makes the index page"""
-    response = client.get("/page2")
+    response = client.get("/docker")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"Docker" in response.data
 
-def test_request_page3(client):
+def test_request_python_flask(client):
     """This makes the index page"""
-    response = client.get("/page3")
+    response = client.get("/python_flask")
     assert response.status_code == 200
-    assert b"Page 3" in response.data
+    assert b"Python and Flask" in response.data
 
 def test_request_page4(client):
     """This makes the index page"""
-    response = client.get("/page4")
+    response = client.get("/contid")
     assert response.status_code == 200
-    assert b"Page 4" in response.data
+    assert b"Continuous Integration and Development" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
